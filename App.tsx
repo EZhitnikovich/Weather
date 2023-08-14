82cfc502d63c4444fee6028d99bd31ac358c25ec
EZhitnikovich <ezhitnikovich@gmail.com>
2023-08-14 12:33:10 +0300

    add background

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
       <StatusBar style="auto" />
+      <Image
+        blurRadius={80}
+        source={require("./assets/images/bg.png")}
+        className="h-full w-full"
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
